b59d0923f590b065750225008e5e9994a6cbf98c
hadis ghafouri <hadisghafouri98@gmail.com>
2021-06-17 12:50:22 +0430

    add easy,medium,hard levels to game

--- script.js
+++ script.js
@@ -4,19 +4,23 @@ const rules = document.getElementById("rules");
 const canvas = document.getElementById("canvas");
 const ctx = canvas.getContext("2d");
 
-const boxModal = document.querySelector('.box-modal');
+const boxModal = document.querySelector(".box-modal");
 
-const boxShadow = document.querySelector('.box-shadow');
+const boxShadow = document.querySelector(".box-shadow");
 
-const noBtn = document.getElementById('noBtn');
-const yesBtn = document.getElementById('yesBtn');
+const noBtn = document.getElementById("noBtn");
+const yesBtn = document.getElementById("yesBtn");
 
-const startGameBtn = document.getElementById('startGameBtn');
+const startGameBtn = document.getElementById("startGameBtn");
 
-const endGameBtn = document.getElementById('endGameBtn');
+const endGameBtn = document.getElementById("endGameBtn");
+const selectList_container = document.querySelector(".game-level");
+const selectList = document.getElementById("game-levels");
+
+let game_level = 0;
 
 let score = 0;
-let isPlaying = 1;
+let isPlaying = 0;
 const brikRowCount = 9;
 const brickColumnCount = 5;
 const delay = 500; //delay to reset the game
@@ -162,26 +166,27 @@ function increaseScore() {
     score++;
     if (score % (brikRowCount * brickColumnCount) === 0) {
         endGame();
-        boxModal.style.display = 'flex';
-        boxShadow.style.display = 'block';
+        boxModal.style.display = "flex";
+        boxShadow.style.display = "block";
+        selectList.style.display = "block";
     }
 }
 
 // Make all bricks appear
 
 function showAllBricks() {
-    bricks.forEach(row => {
-        row.forEach(brick => {
+    bricks.forEach((row) => {
+        row.forEach((brick) => {
             brick.visible = true;
-        })
+        });
     });
 }
 
 function hideAllBricks() {
-    bricks.forEach(row => {
-        row.forEach(brick => {
+    bricks.forEach((row) => {
+        row.forEach((brick) => {
             brick.visible = false;
-        })
+        });
     });
 }
 
@@ -205,46 +210,48 @@ function update() {
 
 // keydown event
 function keyDown(e) {
-    if (e.key === 'Right' || e.key === 'ArrowRight') {
+    if (e.key === "Right" || e.key === "ArrowRight") {
         paddle.dx = paddle.speed;
-    } else if (e.key === 'Left' || e.key === 'ArrowLeft') {
+    } else if (e.key === "Left" || e.key === "ArrowLeft") {
         paddle.dx = -paddle.speed;
     }
 }
 
 function keyUp(e) {
-    if (e.key === 'Right' ||
-        e.key === 'ArrowRight' ||
-        e.key === 'Left' ||
-        e.key === 'ArrowLeft'
+    if (
+        e.key === "Right" ||
+        e.key === "ArrowRight" ||
+        e.key === "Left" ||
+        e.key === "ArrowLeft"
     ) {
         paddle.dx = 0;
     }
 }
 
-document.addEventListener('keydown', keyDown);
-document.addEventListener('keyup', keyUp);
+document.addEventListener("keydown", keyDown);
+document.addEventListener("keyup", keyUp);
 
-rulesBtn.addEventListener('click', () => {
-    rules.classList.add('show');
-})
+rulesBtn.addEventListener("click", () => {
+    rules.classList.add("show");
+});
 
-closeBtn.addEventListener('click', () => {
-    rules.classList.remove('show');
-})
+closeBtn.addEventListener("click", () => {
+    rules.classList.remove("show");
+});
 
-noBtn.addEventListener('click', () => {
-    boxModal.style.display = 'none';
-    boxShadow.style.display = 'none';
-})
+noBtn.addEventListener("click", () => {
+    boxModal.style.display = "none";
+    boxShadow.style.display = "none";
+});
 
-yesBtn.addEventListener('click', () => {
-    boxModal.style.display = 'none';
-    boxShadow.style.display = 'none';
+yesBtn.addEventListener("click", () => {
+    boxModal.style.display = "none";
+    boxShadow.style.display = "none";
     startGame();
-})
+});
 
 function endGame() {
+    enableForm();
     isPlaying = 0;
     ball.visible = false;
     paddle.visible = false;
@@ -253,21 +260,63 @@ function endGame() {
 }
 
 function startGame() {
+    disableForm();
     showAllBricks();
     score = 0;
     paddle.x = canvas.width / 2 - 40;
     paddle.y = canvas.height - 20;
     ball.x = canvas.width / 2;
     ball.y = canvas.height / 2;
-    ball.speed = 5;
-    ball.dx = 5;
-    ball.dy = -5;
+    if (game_level === 0) {
+        //easy
+        ball.speed = 4;
+        ball.dx = 4;
+        ball.dy = -4;
+        paddle.w = 115;
+    } else if (game_level === 1) {
+        //medium
+        ball.speed = 5;
+        ball.dx = 5;
+        ball.dy = -5;
+        paddle.w = 100;
+    } else if (game_level === 2) {
+        //hard
+        ball.speed = 6;
+        ball.dx = 6;
+        ball.dy = -6;
+        paddle.w = 95;
+    }
+
     ball.visible = true;
     paddle.visible = true;
     isPlaying = 1;
     update();
 }
 
-startGameBtn.addEventListener('click', startGame);
+function disableForm() {
+    for (option of selectList.options) {
+        option.disabled = true;
+    }
+}
 
-endGameBtn.addEventListener('click', endGame);
+function enableForm() {
+    for (option of selectList.options) {
+        option.disabled = false;
+    }
+}
+
+startGameBtn.addEventListener("click", startGame);
+
+endGameBtn.addEventListener("click", endGame);
+
+selectList.addEventListener("change", () => {
+    let i = selectList.selectedIndex;
+    const state = selectList.options[i].text;
+    if (state === "easy") {
+        game_level = 0;
+    } else if (state === "medium") {
+        game_level = 1;
+    } else if (state === "hard") {
+        game_level = 2;
+    }
+});
